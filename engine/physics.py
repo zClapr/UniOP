e1880e292a24cc477e83b6742326fe0df8bfcc07
h-zhong <48451623+zClapr@users.noreply.github.com>
2021-09-17 16:57:17 +0200

    Moving bodies attempt, need to fix color glitch + top layer latency

--- engine/physics.py
+++ engine/physics.py
@@ -6,6 +6,39 @@ from pyglet.gl import GL_TRIANGLE_STRIP
 
 import random
 
+def GetSphere(r, pos:list, texture):
+    resolution = 10
+    layers = []
+    latiLayerAngles = floatRange(-90, 90, 180/resolution)
+
+    for latiLayerAngle in latiLayerAngles:
+        vertices, textures = [], []
+        # color = self.color
+        # color[color.index(max(color))] = int(
+        #     color[color.index(max(color))] +
+        #     (latiLayerAngles.index(latiLayerAngle)/len(latiLayerAngles)*15)
+        # )
+        # self.color[self.color.index(max(self.color))] += random.randint(-30,30)
+
+        for longLayerAngle in floatRange(-180, 180, 360/resolution):
+            x = -cos(radians(latiLayerAngle)) * cos(radians(longLayerAngle)) * r + pos[0]
+            y = sin(radians(latiLayerAngle)) * r + pos[1]
+            z = cos(radians(latiLayerAngle)) * sin(radians(longLayerAngle)) * r + pos[2]
+
+            vertices += [x,y,z]
+            textures += texture
+
+            x = -cos(radians((latiLayerAngle+(180/resolution)))) * cos(radians(longLayerAngle)) * r + pos[0]
+            y = sin(radians((latiLayerAngle+(180/resolution)))) * r + pos[1]
+            z = cos(radians((latiLayerAngle+(180/resolution)))) * sin(radians(longLayerAngle)) * r + pos[2]
+            
+            vertices += [x,y,z]
+            textures += texture
+        
+        layers.append([vertices, textures])
+
+    return layers
+
 class celestrial_body:
     def __init__(self, mass:float, position:list, radius:float=None, density:float=None, color:list=[56, 163, 42]):
         self.mass = mass
@@ -27,45 +60,8 @@ class celestrial_body:
             else:
                 self.density = density
                 self.radius = (3*((mass*density)/(4*pi))**1/3)
-    
-    def get_layers(self):
-        resolution = 10
-        coordinates = []
-        layers = []
         
-        for c in self.position:
-            coordinates.append(c)
-
-        for latiLayerAngle in floatRange(-90, 90, 180/resolution):
-            vertices, textures = [], []
-
-            self.color[self.color.index(max(self.color))] += 15
-
-            for longLayerAngle in floatRange(-180, 180, 360/resolution):
-                x = -cos(radians(latiLayerAngle)) * cos(radians(longLayerAngle)) * self.radius + coordinates[0]
-                y = sin(radians(latiLayerAngle)) * self.radius + coordinates[1]
-                z = cos(radians(latiLayerAngle)) * sin(radians(longLayerAngle)) * self.radius + coordinates[2]
-
-                vertices += [x,y,z]
-                textures += self.color
-
-                x = -cos(radians((latiLayerAngle+(180/resolution)))) * cos(radians(longLayerAngle)) * self.radius + coordinates[0]
-                y = sin(radians((latiLayerAngle+(180/resolution)))) * self.radius + coordinates[1]
-                z = cos(radians((latiLayerAngle+(180/resolution)))) * sin(radians(longLayerAngle)) * self.radius + coordinates[2]
-                
-                vertices += [x,y,z]
-                textures += self.color
-            
-            layers.append([vertices, textures])
-
-        return layers
-
-    def draw(self):
-        for c in self.position:
-            self.position[self.position.index(c)] += random.randint(-5,5)
-
-        if self.layers ==[]:
-            for layer in self.get_layers():
+        for layer in self.get_layers():
                 vertices, textures = layer[0], layer[1]
 
                 l = self.batch.add(
@@ -74,9 +70,13 @@ class celestrial_body:
                 )
 
                 self.layers.append(l)
-        else:
-            for layer_number in range(0, len(self.layers)-1):
-                self.layers[layer_number].vertices[:] = self.get_layers()[layer_number][0]
+
+    def draw(self):
+        for c in self.position:
+            self.position[self.position.index(c)] += random.randint(-1,1)
+
+        for layer_number in range(0, len(self.layers)-1):
+            self.layers[layer_number].vertices[:] = self.get_layers()[layer_number][0]
             
         self.batch.draw()
     
